ce7bc556558f623f004d8b3d311b8e920b5e57da
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-11-13 12:52:21 +0100

    Scene changer

--- fischl/Assets/SceneChanger.cs
+++ fischl/Assets/SceneChanger.cs
@@ -5,20 +5,25 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     [SerializeField] String[] scenes;
+    private bool canChange;
     private int i = 0;
 
     private void Start() {
         DontDestroyOnLoad(gameObject);
+        canChange = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!canChange) return;
         if(Input.GetKeyDown(KeyCode.RightArrow)) {
             i++;
             if(i >= scenes.Length) i = 0;
             Debug.Log("Loading " + scenes[i]);
             SceneManager.LoadScene(scenes[i]);
+            canChange = false;
+            Invoke("EnableChange", 5f);
         }
 
         if(Input.GetKeyDown(KeyCode.LeftArrow)) {
@@ -26,6 +31,12 @@ public class SceneChanger : MonoBehaviour
             if(i < 0) i = scenes.Length - 1;
             Debug.Log("Loading " + scenes[i]);
             SceneManager.LoadScene(scenes[i]);
+            canChange = false;
+            Invoke("EnableChange", 5f);
         }
+    }
+
+    private void EnableChange() {
+        canChange = true;
     }
 }
