4d35683d4b50f9bc4720ed8ae0144c383a209c87
hebabz <heba.bouzidi@etu.unice.fr>
2023-11-20 08:57:39 +0100

    Enhanced the code's quality of Assemble!

--- fischl/Assets/Scenes/Assemble!/PuzzleManager.cs
+++ fischl/Assets/Scenes/Assemble!/PuzzleManager.cs
@@ -5,34 +5,33 @@ using UnityEngine.EventSystems;
 
 public class PuzzleManager : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    public GameObject selectedObject;
-    public Vector3 originalPosition;
-    public GameObject droppingArea;
     public GameObject correctSpot;
-    public GameObject[] answersSpot;
+    
+    private GameObject selectedObject;
+    private Vector3 originalPosition;
+    private GameObject droppingArea;
+    private GameObject[] answersSpot;
 
     public void Start()
     {
-        originalPosition = selectedObject.transform.position;
+        answersSpot = GameObject.FindGameObjectsWithTag("Placeholder");
+        droppingArea = correctSpot.transform.parent.gameObject;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         selectedObject = eventData.pointerCurrentRaycast.gameObject;
+        originalPosition = selectedObject.transform.position;
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-    }
+    public void OnBeginDrag(PointerEventData eventData){}
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("dragging!!");
         if (eventData.position.x > 0 && eventData.position.x < Screen.width && eventData.position.y > 0 && eventData.position.y < Screen.height)
         {
             selectedObject.transform.position = eventData.position;
         }
-
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -43,47 +42,50 @@ public class PuzzleManager : MonoBehaviour, IPointerDownHandler, IBeginDragHandl
         }
     }
 
+    private float[] GetCoordinates(GameObject theObject){
+        //retrieve the object coordinates according to world space
+        Vector3 objectLeftBottom = theObject.transform.TransformPoint(theObject.GetComponent<RectTransform>().rect.min);
+        Vector3 objectRightTop = theObject.transform.TransformPoint(theObject.GetComponent<RectTransform>().rect.max);
+
+        //retrieve the object coordinates according to screen space
+        Vector3 objectLeftBottomScreen = Camera.main.WorldToScreenPoint(objectLeftBottom);
+        Vector3 objectRightTopScreen = Camera.main.WorldToScreenPoint(objectRightTop);
+
+        //retrieve the object coordinates according to screen space
+        float objectLeft = objectLeftBottomScreen.x;
+        float objectBottom = objectLeftBottomScreen.y;
+        float objectRight = objectRightTopScreen.x;
+        float objectTop = objectRightTopScreen.y;
+
+        return new float[] {objectLeft, objectBottom, objectRight, objectTop};
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        //retrieve the droppingarea coordinates according to world space
-        Vector3 droppingAreaLeftBottom = droppingArea.transform.TransformPoint(droppingArea.GetComponent<RectTransform>().rect.min);
-        Vector3 droppingAreaRightTop = droppingArea.transform.TransformPoint(droppingArea.GetComponent<RectTransform>().rect.max);
-
+        float[] droppingAreaCoordinates = GetCoordinates(droppingArea);
         //retrieve the droppingarea coordinates according to screen space
-        Vector3 droppingAreaLeftBottomScreen = Camera.main.WorldToScreenPoint(droppingAreaLeftBottom);
-        Vector3 droppingAreaRightTopScreen = Camera.main.WorldToScreenPoint(droppingAreaRightTop);
+        float droppingAreaLeft = droppingAreaCoordinates[0];
+        float droppingAreaBottom = droppingAreaCoordinates[1];
+        float droppingAreaRight = droppingAreaCoordinates[2];
+        float droppingAreaTop = droppingAreaCoordinates[3];
 
-        //retrieve the droppingarea coordinates according to screen space
-        float droppingAreaLeft = droppingAreaLeftBottomScreen.x;
-        float droppingAreaBottom = droppingAreaLeftBottomScreen.y;
-        float droppingAreaRight = droppingAreaRightTopScreen.x;
-        float droppingAreaTop = droppingAreaRightTopScreen.y;
-
-        //if the selectedObjetc is not within the limits or outside the screen area
+        //if the selectedObject is not within the limits or outside the screen area
         if (eventData.position.x < droppingAreaLeft || eventData.position.x > droppingAreaRight || eventData.position.y < droppingAreaBottom || eventData.position.y > droppingAreaTop)
         {
             selectedObject.transform.position = originalPosition;
-        }
-        else
-        {
+        } else {
             selectedObject.transform.position = eventData.position;
             //update original position
             originalPosition = eventData.position;
         }
-         
-        //retrieve the correctSpot coordinates according to world space
-        Vector3 correctSpotLeftBottom = correctSpot.transform.TransformPoint(correctSpot.GetComponent<RectTransform>().rect.min);
-        Vector3 correctSpotRightTop = correctSpot.transform.TransformPoint(correctSpot.GetComponent<RectTransform>().rect.max);
+        
+        float[] correctSpotCoordinates = GetCoordinates(correctSpot);
 
         //retrieve the correctSpot coordinates according to screen space
-        Vector3 correctSpotLeftBottomScreen = Camera.main.WorldToScreenPoint(correctSpotLeftBottom);
-        Vector3 correctSpotRightTopScreen = Camera.main.WorldToScreenPoint(correctSpotRightTop);
-
-        //retrieve the correctSpot coordinates according to screen space
-        float correctSpotLeft = correctSpotLeftBottomScreen.x;
-        float correctSpotBottom = correctSpotLeftBottomScreen.y;
-        float correctSpotRight = correctSpotRightTopScreen.x;
-        float correctSpotTop = correctSpotRightTopScreen.y;
+        float correctSpotLeft = correctSpotCoordinates[0];
+        float correctSpotBottom = correctSpotCoordinates[1];
+        float correctSpotRight = correctSpotCoordinates[2];
+        float correctSpotTop = correctSpotCoordinates[3];
 
         //retrieve the correctSpot's middle coordinates
         Vector3 correctSpotMiddle = correctSpot.transform.TransformPoint(correctSpot.GetComponent<RectTransform>().rect.center);
@@ -92,7 +94,7 @@ public class PuzzleManager : MonoBehaviour, IPointerDownHandler, IBeginDragHandl
         float offsetX = 0.5f;
         float offsetY = 0.5f;
 
-        //if the selectedobject is within the limits of the correctSpot turn th spot green
+        //if the selectedobject is within the limits of the correctSpot area
         if (eventData.position.x > correctSpotLeft + offsetX && eventData.position.x < correctSpotRight - offsetX && eventData.position.y > correctSpotBottom + offsetY && eventData.position.y < correctSpotTop - offsetY)
         {
             //replace it correctly in the middle of correctspot
