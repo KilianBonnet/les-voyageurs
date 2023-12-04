b5577b01c504bdc916b4ffc352c04ed5556d0c5a
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2023-12-04 16:34:31 +0100

    Skeleton + animations

--- nahida/Assets/Packages/BowRelia/EnemyScript.cs
+++ nahida/Assets/Packages/BowRelia/EnemyScript.cs
@@ -11,11 +11,13 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] float speed = 5f;
     private Rigidbody rb;
     public float detectionRange = 20f;
-    public float minDistance = 1f;
+    public float minDistance = 1.735f;
+    private Animator animator;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,25 +29,42 @@ public class EnemyScript : MonoBehaviour
             {
                 
                 float distance = Vector3.Distance(transform.position, player.position);
-                if(distance > minDistance)
-                    moveToPlayer();
-                lookAt();
+                if (distance > minDistance)
+                    MoveToPlayer();
+                else
+                {
+                    rb.velocity = Vector3.zero;
+                    animator.SetBool("walking", false);
+                    
+                    AttackPlayer();
+                }
                 break;
             }
         }  
     }
 
-    void lookAt()
+    void LookAt()
     {
-        transform.LookAt(player);
+        Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(targetPosition);
     }
     
-    void moveToPlayer()
+    void MoveToPlayer()
     {
         Vector3 pos = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         rb.MovePosition(pos);
+        animator.SetBool("walking", true);
+        LookAt();
     }
 
+    void AttackPlayer()
+    {
+        int randomValue = Random.Range(0, 2);
+
+        animator.SetBool("attacking1", (randomValue == 0));
+        animator.SetBool("attacking2", (randomValue == 1));
+
+    }
 
 
 }
