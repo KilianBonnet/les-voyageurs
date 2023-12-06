b54ed1defead7395403b44397a7ebc86afeabd6f
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-06 21:13:00 +0100

    Bonus

--- fischl/Assets/Component/Bowrelia/Bonus/BonusEventHandler.cs
+++ fischl/Assets/Component/Bowrelia/Bonus/BonusEventHandler.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public struct BonusHandlerStruct {
+    public string bonusName;
+    public ElementContainer bonusContainer;
+}
 
 public class BonusEventHandler : MonoBehaviour
 {
-    [SerializeField] private ElementContainer bombContainer;
+    [SerializeField] private List<BonusHandlerStruct> bonusHandlerList;
     [SerializeField] private Animator wheelAnimator;
-    
+
+    private Notification notification;
     private AudioSource bonusAudio;
 
     private void Start() {
+        notification = FindObjectOfType<Notification>();
         NetworkingBonus.BonusEvent += OnBonusEvent;
         bonusAudio = GetComponent<AudioSource>();
     }
@@ -16,10 +25,10 @@ public class BonusEventHandler : MonoBehaviour
         bonusAudio.Play();
         wheelAnimator.SetTrigger("Notification");
 
-        switch(bonus) {
-            case BonusType.BOMB:
-                bombContainer.IncreaseCount(1);
-                break;
+        foreach (BonusHandlerStruct item in bonusHandlerList)
+        {
+            item.bonusContainer.IncreaseCount(1);
+            notification.PlayNotification($"Obtained\n{item.bonusName}!", item.bonusName);
         }
     }
 }
