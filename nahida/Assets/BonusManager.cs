2fe3e6d16612f4fd87eb09038b3bf27c2f000861
hebabz <heba.bouzidi@etu.unice.fr>
2023-12-08 16:42:09 +0100

    Bonus animations over

--- nahida/Assets/BonusManager.cs
+++ nahida/Assets/BonusManager.cs
@@ -10,6 +10,29 @@ public class BonusManager : MonoBehaviour
     {
         bomb = transform.parent.Find("Bomb").gameObject;
     }
+
+    public void Update()
+    {
+        if(bomb)
+        {
+            if (bomb.GetComponent<Animator>().GetBool("isPositioned"))
+            {
+                Debug.Log("Send bomb to the table!");
+                bomb.GetComponent<Animator>().SetBool("isSentToTable", true);
+            }
+
+            if (bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("LiftUp") && bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                bomb.GetComponent<Animator>().SetBool("isPositioned", true);
+            }
+
+            if (bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("FadeOut") && bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                Destroy(bomb);
+            }
+        }
+    }
+
     public void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.name == "Lid")
