b233efcbba51782409df196bb0a41b924a6eb885
AureliaChabanier <aurelia.chabanier@etu.unice.fr>
2023-12-11 16:16:14 +0100

    Red zone when enemy touches

--- fischl/Assets/Component/Bowrelia/Map/VrMap.cs
+++ fischl/Assets/Component/Bowrelia/Map/VrMap.cs
@@ -1,14 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class VrMap : MonoBehaviour
 {
     [SerializeField] private ScoreManager scoreManager;
+    private GameObject touched = GameObject.Find("Touched");
+
 
     private void OnTriggerEnter2D(Collider2D other) {
 
         if(other.transform.CompareTag("Enemy")) {
             Destroy(other.gameObject);
             scoreManager.IncreaseScore(-200);
+            ShowAndHideObject(touched, 3.0f);
         }
+    }
+
+    private IEnumerator ShowAndHideObject(GameObject obj, float duration)
+    {
+        float timePassed = 0f;
+        while (timePassed < duration)
+        {
+            obj.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            obj.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            timePassed += 1.0f;
+        }
+        obj.SetActive(false);
     }
 }
