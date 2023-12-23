1b712719db7eddc4e3677aa3792c2c4f608ea3eb
hebabz <heba.bouzidi@etu.unice.fr>
2023-12-23 16:56:54 +0100

    Added a portal to send the bomb to

---
+++ nahida/Assets/Resources/BowRelia/BonusManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+
+public class BonusManager : MonoBehaviour
+{
+    [SerializeField] RoomManager roomManager;
+    private GameObject bomb;
+    private GameObject portal;
+
+    public void Start()
+    {
+        bomb = transform.parent.Find("Bomb").gameObject;
+        portal = transform.parent.Find("Portal blue").gameObject;
+    }
+
+    public void Update()
+    {
+        if(bomb)
+        {
+            if (bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("LiftUp") && bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                bomb.GetComponent<Animator>().SetBool("isPositioned", true);
+            }
+
+            if (bomb.GetComponent<Animator>().GetBool("isPositioned"))
+            {
+                bomb.GetComponent<Grabbable>().enabled = true;
+                bomb.GetComponent<HandGrabInteractable>().enabled = true;
+            }
+
+            if (bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("FadeIntoPortal") && bomb.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                Destroy(bomb);
+                roomManager.OpenDoors();
+                NetworkingBonus.SendBonusEvent(BonusType.BOMB);
+            }
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Lid")
+        {
+            bomb.GetComponent<Animator>().SetBool("isLidOpen", true);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Lid")
+        {
+            bomb.GetComponent<Animator>().SetBool("isLidOpen", false);
+        }
+        if(collision.gameObject.name == "Portal blue")
+        {
+            bomb.GetComponent<Animator>().SetBool("isSentToTable", true);
+            //once this animation is done, add FadeIntoPortal animation to the portal
+            GameObject portal = collision.gameObject;
+            portal.gameObject.GetComponent<Animator>().SetBool("isSentToTable", true);
+            if(portal.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("FadeIntoPortal") && portal.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                Destroy(portal);
+            }
+        }
+    }
+}
