5e7ecdc140ac0d19dcba7fe81bc376c8a9df3ea6
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2024-01-08 15:10:51 +0100

    Skeleton sound

---
+++ nahida/Assets/Resources/Sound/EnemyMusicManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class EnemyMusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource source;
+    [SerializeField]
+    private AudioClip idle;
+    [SerializeField]
+    private AudioClip walking;
+    [SerializeField]
+    private AudioClip hurt1;
+    [SerializeField]
+    private AudioClip hurt2;
+
+
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void IdleReplay()
+    {
+        if (!source.isPlaying)
+        {
+            if (idle)
+            {
+                source.clip = idle;
+                source.PlayScheduled(4);
+            }
+            
+        }
+    }
+
+    public void Hurt1()
+    {
+        if (!source.isPlaying)
+        {
+            if (hurt1)
+            {
+                source.clip = hurt1;
+                source.Play();
+            }
+
+        }
+    }
+
+    public void Hurt2()
+    {
+        if (!source.isPlaying)
+        {
+            if (hurt2)
+            {
+                source.clip = hurt2;
+                source.Play();
+            }
+
+        }
+    }
+
+    public void Walking()
+    {
+        if (!source.isPlaying)
+        {
+            if (walking)
+            {
+                source.clip = walking;
+                source.PlayScheduled(1);
+            }
+
+        }
+    }
+}
