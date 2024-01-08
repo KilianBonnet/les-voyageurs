aaba9c817895362282bc56e75cf8f2099e27982f
KilianBonnet <90778036+KilianBonnet@users.noreply.github.com>
2024-01-08 09:54:09 +0100

    J'essaye de faire une épée mal

--- nahida/Assets/Networking/Networking Objects/NetworkingRoom.cs
+++ nahida/Assets/Networking/Networking Objects/NetworkingRoom.cs
@@ -5,22 +5,25 @@ public class NetworkingRoom : MonoBehaviour
 {
     public static event Action<int> RoomChangeEvent;
 
-    private void Start() {
+    private void Start()
+    {
         WebSocketClient.OnSocketMessage += OnSocketMessage;
     }
 
-    private void OnSocketMessage(SocketMessage socketMessage) {
-        if(socketMessage.op != SocketOP.ROOM_EVENT)
+    private void OnSocketMessage(SocketMessage socketMessage)
+    {
+        if (socketMessage.op != SocketOP.ROOM_EVENT)
             return;
 
         RoomData d = socketMessage.d.ToObject<RoomData>();
         RoomChangeEvent.Invoke(d.room);
     }
 
-    static public void SendRoomEvent(int room) {
-        /*WebSocketClient.Instance.SendMessage(
-            SocketOP.ROOM_EVENT, 
-            new { room });*/
+    static public void SendRoomEvent(int room)
+    {
+        WebSocketClient.Instance.SendMessage(
+            SocketOP.ROOM_EVENT,
+            new { room });
     }
 
 }
--- nahida/Assets/Resources/BowRelia/EntryRoomTrigger.cs
+++ nahida/Assets/Resources/BowRelia/EntryRoomTrigger.cs
@@ -6,7 +6,8 @@ public class EntryRoomTrigger : MonoBehaviour
     public static event Action<int> PlayerRoomEnterEvent;
     private int roomId;
 
-    private void Start() {
+    private void Start()
+    {
         roomId = int.Parse(name.Split(" ")[1]);
     }
 
